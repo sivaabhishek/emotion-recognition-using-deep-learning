34c2a0c4402a0401c4b18df4080d435ea44734d5
S. Siva Abhishek <60132530+sivaabhishek@users.noreply.github.com>
2021-03-09 14:55:50 +0530

    Update emotions.py

--- emotions.py
+++ emotions.py
@@ -9,11 +9,13 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
+
 # plots accuracy and loss curves
 def plot_model_history(model_history):
     """
     Plot Accuracy and Loss curves given the model_history
     """
+    # print(model_history.history.keys())
     fig, axs = plt.subplots(1, 2, figsize=(15, 5))
     # summarize history for accuracy
     axs[0].plot(range(1, len(model_history.history['accuracy']) + 1), model_history.history['accuracy'])
@@ -87,6 +89,42 @@ model.add(Dense(7, activation='softmax'))
 # Layers involved in the model
 model.summary()
 
+def show():
+    model.load_weights( 'model.h5' )
+
+    # prevents openCL usage and unnecessary logging messages
+    cv2.ocl.setUseOpenCL( False )
+
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    # start the webcam feed
+    cap = cv2.VideoCapture( 0 )
+    while True:
+        # Find haar cascade to draw bounding box around face
+        ret, frame = cap.read()
+        if not ret:
+            break
+        facecasc = cv2.CascadeClassifier( 'haarcascade_frontalface_default.xml' )
+        gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
+        faces = facecasc.detectMultiScale( gray, scaleFactor=1.3, minNeighbors=7 )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle( frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2 )
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims( np.expand_dims( cv2.resize( roi_gray, (48, 48) ), -1 ), 0 )
+            prediction = model.predict( cropped_img )
+            maxindex = int( np.argmax( prediction ) )
+            cv2.putText( frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
+                         2, cv2.LINE_AA )
+
+        cv2.imshow( 'Video', cv2.resize( frame, (1600, 960), interpolation=cv2.INTER_CUBIC ) )
+        if cv2.waitKey( 1 ) & 0xFF == ord( 'q' ):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 mode = input("Train -t, Display -d: ")
 
 # If you want to train the same model or try other models, go for this
@@ -100,40 +138,9 @@ if mode == "t":
         validation_data=validation_generator,
         validation_steps=num_val // batch_size)
     model.save_weights('model.h5')
-    # plot_model_history(model_info)
+    plot_model_history(model_info)
+    show()
 
 # emotions will be displayed on your face from the webcam feed
 elif mode == "d":
-    model.load_weights('model.h5')
-
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
-
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-
-    # start the webcam feed
-    cap = cv2.VideoCapture(0)
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=7)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255),
-                        2, cv2.LINE_AA)
-
-        cv2.imshow('Video', cv2.resize(frame, (1600, 960), interpolation=cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    show()
