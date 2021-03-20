e093271a060742ce482f0927862c7b7990779140
S. Siva Abhishek <60132530+sivaabhishek@users.noreply.github.com>
2021-03-20 13:57:56 +0530

    Update time_scheduled_running.py

--- time_scheduled_running.py
+++ time_scheduled_running.py
@@ -3,7 +3,7 @@ import os
 import cv2
 
 while True:
-    os.system("python new.py")
+    os.system("python Capture_and_save_photo.py")
     path = r'C:\Users\sivaa\PycharmProjects\Projects\Emotion Detection\src\ss.png'
     image = cv2.imread( path )
     cv2.imshow("image",image)
